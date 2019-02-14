b02dccfbf44bbb49e8e1fd17bc077788d6b275b4
Robert Wood Jr <rdrwood@gmail.com>
2019-02-14 10:40:40 -0800

    added comments test code and code that makes it work

--- Prog6VectorPalindrome/Main.cpp
+++ Prog6VectorPalindrome/Main.cpp
@@ -21,6 +21,19 @@ int main() {
 	cout << boolalpha << isPalindrome(v1True) << endl;
 	vector<int> v2True{ 1, 2, 3, 3, 2, 1 };
 	cout << boolalpha << isPalindrome(v2True) << endl;
+
+	// code i added for testing sake
+	//vector<int> v4True{ 1, 2, 3, 4, 2, 1 };
+	//cout << boolalpha << isPalindrome(v4True) << endl;
+	//vector<int> v3True{ 1, 2, 3, 3, 3, 1 };
+	//cout << boolalpha << isPalindrome(v3True) << endl;
+	//vector<int> v6True{ 1, 2, 3, 4, 4, 3, 2, 1 };
+	//cout << boolalpha << isPalindrome(v6True) << endl;
+	//vector<int> v8True{ 1, 1 };
+	//cout << boolalpha << isPalindrome(v8True) << endl;
+	//vector<int> v9True{ 1 };
+	//cout << boolalpha << isPalindrome(v9True) << endl;
+
 	system("pause");
 	return 0;
 }
@@ -35,7 +48,11 @@ bool isPalindrome(const vector<int> & v) {
 	// as long as front and rear elements have the same value
 	// and front hasn't gone past rear, increment both.
 	for (; *front == *rear && front < rear; ++front, --rear) {
+		// *front = value  front = position
+	}
 
+	if ( *front == *rear ) {
+		return true;
 	}
 	
 	// check positions of front and rear.
